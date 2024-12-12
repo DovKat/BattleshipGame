02ea19199acf600dd99914d2ed4fd8deea1ff507
DovKat <dovkat1@ktu.lt>
2024-12-12 17:54:43 +0200

    fixes

--- battleship-api/Program.cs
+++ battleship-api/Program.cs
@@ -1,32 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Register services
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowHost",
         policy =>
         {
-            policy.WithOrigins("https://salmon-meadow-08f848403.4.azurestaticapps.net")
+            policy.SetIsOriginAllowed(origin => 
+                                       new Uri(origin).Host == "https://salmon-meadow-08f848403.4.azurestaticapps.net/")
                 .AllowAnyHeader()
-                .AllowAnyMethod()
-                .AllowCredentials()
-                .WithExposedHeaders("Content-Length");
+                .AllowAnyMethod();
         });
 });
+
 
 builder.Services.AddSignalR();
 
 var app = builder.Build();
 
-// Use middleware
-app.UseHttpsRedirection();
-app.UseCors("AllowHost");
 
-app.Use(async (context, next) =>
-{
-    Console.WriteLine($"Request Path: {context.Request.Path}");
-    await next.Invoke();
-});
+app.UseCors("AllowHost"); 
+
+app.UseHttpsRedirection();
+app.UseRouting();
+
 
 app.MapHub<GameHub>("/gameHub");
 
