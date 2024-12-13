adc2cbaa95160bd3fb8accf1c24d07e27e8c44fb
DovKat <dovkat1@ktu.lt>
2024-12-13 02:02:17 +0200

    added signalr hub

--- battleship-api/Program.cs
+++ battleship-api/Program.cs
@@ -6,19 +6,19 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowHost", policy =>
     {
-        policy
-            .WithOrigins("http://localhost:3000") // Replace with your frontend URL
-            .AllowAnyHeader()
-            .AllowAnyMethod()
-            .AllowCredentials()
-            .WithExposedHeaders("Content-Length");
-    });
-})
-    .AddSignalR();
-
+        options.AddPolicy("AllowHost", policy =>
+        {
+            policy
+                .WithOrigins("https://salmon-meadow-08f848403.4.azurestaticapps.net") // Replace with your frontend URL
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials()
+                .WithExposedHeaders("Content-Length");
+        });
+    })
+    .AddSignalR() //https://battleshipgamehub.service.signalr.net
+    .AddAzureSignalR(options => options.ConnectionString = "Endpoint=https://battleshipgamehub.service.signalr.net;AccessKey=FOZrHncHR81uX9kwxjAo3BRPxw0RaG67Q3A3R6wancwYbcHJwOiEJQQJ99ALACi5YpzXJ3w3AAAAASRS81Y0;Version=1.0;");
 var app = builder.Build();
 
 // Apply CORS policy before other middleware
@@ -28,11 +28,12 @@ app.UseHttpsRedirection();
 // Ensure routing comes after CORS middleware
 app.UseRouting();
 
+// SignalR hub mapping
+app.MapHub<GameHub>("/gameHub").RequireCors("AllowHost");
 app.UseEndpoints(endpoints =>
-    {
-        endpoints.MapHub<GameHub>("/gameHub");
-    });
-
+{
+    endpoints.MapHub<GameHub>("/gameHub");
+});
 // Run the app
 app.Run();
 
